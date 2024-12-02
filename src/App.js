e2b586fc763a1cdda4e85c6ff5e9e078943cd1ea
hitesh_nathani <hitesh.nathani@groovyweb.co>
2024-12-02 22:28:25 +0530

    "update: index and app js"

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Home from "./screens/Home";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import { CartProvider } from "./components/Cards/ContextReducer";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
